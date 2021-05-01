2073651f39684bbb4d74c90d9193b95f1f569aa8
dlanoronald123 <dlanoronald123@gmail.com>
2021-05-01 23:36:10 +0800

    samples

---
+++ samples.rb
@@ -0,0 +1,36 @@
+# def add_three(number)
+#     number + 3
+#   end
+  
+#   returned_value = add_three(4)
+#   puts returned_value
+
+
+# def add_three(n)
+#     new_value = n + 3
+#     puts new_value
+#     new_value
+#   end
+
+#   add_three(7)
+
+# def first
+#     puts "first method"
+#   end
+  
+#   def second
+#     first
+#     puts "second method"
+#   end
+  
+#   second
+#   puts "main method"
+
+
+  def scream(words)
+    words = words + "!!!!"
+    return
+    puts words
+  end
+  
+  scream("Yippeee")
