c4d1afd7abda39868e5b8b4aed16fbce7842e744
dlanoronald123 <dlanoronald123@gmail.com>
2021-05-04 20:17:30 +0800

    class_activity

--- class_activity.rb
+++ class_activity.rb
@@ -1,16 +1,42 @@
 
+# class Book
+#     def initialize(name,author,pages)
+#       @name = name
+#       @author = author
+#       @pages = pages
+#     end
+  
+#     def greet
+#       puts "The book #{@name} created by #{@author} has #{@pages} pages"
+#     end
+#   end
+  
+  
+#   harry_potter = Book.new("Deathly Hollows","JK Rowling",500)
+#   harry_potter.greet
+
+
+  
 class Book
-    def initialize(name,author,pages)
-      @name = name
-      @author = author
-      @pages = pages
-    end
-  
-    def greet
-      puts "The book #{@name} created by #{@author} has #{@pages} pages"
-    end
+  attr_accessor :pages, :author
+  def initialize(name,author,pages)
+    @name = name
+    @author = author
+    @pages = pages
   end
-  
-  
-  harry_potter = Book.new("Deathly Hollows","JK Rowling",500)
-  harry_potter.greet
+
+  def greet
+    puts "The book #{@name} created by #{@author} has #{@pages} pages"
+  end
+end
+
+
+harry_potter = Book.new("Deathly Hollows","JK Rowling",500)
+harry_potter.greet
+
+puts harry_potter.author
+
+harry_potter.pages = 600
+puts harry_potter.pages
+
+
