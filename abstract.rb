cbf97d6a33b4f3305f9fa2cef26041dde1133037
dlanoronald123 <dlanoronald123@gmail.com>
2021-05-06 21:46:45 +0800

    abstract

--- abstract.rb
+++ abstract.rb
@@ -1,24 +1,3 @@
-# class Person
-
-#     def initialize(name, year_born, year_today)
-#       @name = name
-#       @year_born = year_born
-#       @year_today = year_today
-#     end
-  
-#     def know_future_age(year)
-#       return age_calculator + (year - @year_today)
-#     end
-  
-#     private
-#     def age_calculator
-#       return @year_today - @year_born
-#     end
-#   end
-  
-#   person1 = Person.new("Eli", 1990, 2021)
-#   p person1.know_future_age(2026)
-
 class Area
     def initialize(shape,length, width)
         @shape = shape
