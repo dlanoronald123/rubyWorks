59b836e7a62115f1486cc68600f79913aa6cd97d
dlanoronald123 <dlanoronald123@gmail.com>
2021-05-10 19:02:22 +0800

    positive_negative

--- positive_negative.rb
+++ positive_negative.rb
@@ -1,17 +1,16 @@
-def count_sum(array)
-    positive = 0
-    negative = 0
-  
-    array.each do |num|
-      if num < 0
-        negative += num
-      else
-        positive += 1
-      end
-    end
-  
-    [positive, negative]
+def count_positives_sum_negatives(array)
+  if array.to_a.empty?
+    return []
   end
-
-
-  count_sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
+  positive = 0
+  negative = 0
+  array.each { |num|
+    if num > 0
+      positive += 1
+    else
+      negative += num
+    end
+  }
+  newArray = Array[positive, negative]
+  newArray
+end
