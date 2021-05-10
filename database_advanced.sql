bf1e91a9e2809e82c015247c3044932760303abd
dlanoronald123 <dlanoronald123@gmail.com>
2021-05-10 20:24:57 +0800

    database_advanced

---
+++ database_advanced.sql
@@ -0,0 +1,19 @@
+CREATE TABLE classrooms (id INTEGER PRIMARY key not NULL,student_id INTEGER, section VARCHAR(10))
+
+INSERT INTO classrooms(id, student_id, section)
+VALUES(2, 2, 'A'),
+      (3, 3, 'B'),
+      (4, 4, 'C'),
+      (5, 5, 'B'),
+      (6, 6, 'A'),
+      (7, 7, 'C'),
+      (8, 8, 'B'),
+      (9, 9, 'B'),
+      (10, 10, 'C');
+
+
+
+SELECT  * FROM students AS s INNER JOIN classrooms AS c ON c.student_id  = s.id;
+SELECT  * FROM students AS s LEFT JOIN classrooms AS c ON c.id  = s.id;
+SELECT  * FROM students AS s RIGHT JOIN classrooms AS c ON c.id  = s.id;
+SELECT  * FROM students AS s FULL JOIN classrooms AS c ON c.id  = s.id;
