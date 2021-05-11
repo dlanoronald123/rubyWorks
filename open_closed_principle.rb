6f1f85f70003f81b3d204b0f4bd3d51187119261
dlanoronald123 <dlanoronald123@gmail.com>
2021-05-11 19:53:01 +0800

    open_closed

--- open_closed_principle.rb
+++ open_closed_principle.rb
@@ -1,35 +1,78 @@
-class TuitionReport
-    def initialize(name, total)
-        @name = name
-        @total = total
+#Without Open Closed Principle
+
+class SchoolSupplies
+    def initialize(item, quantity, price)
+        @item = item
+        @quantity = quantity
+        @price = price    
+    end
+
+    def select_item
+        case @item
+        when "notebook"
+            compute_notebook_total()
+        when "book"
+            compute_book_total()
+        when "folder"
+            compute_folder_total()
+        else  
+            "We're out of stock"  
+        end
+    end
+
+   private
+       def compute_notebook_total()
+           @quantity * @price
+       end
+       def compute_book_total()
+           @quantity * @price 
+       end
+       def compute_folder_total()
+           @quantity * @price 
+       end
+   end
+
+   
+   
+#With Open Closed Principle
+
+class SchoolSupplies
+    def initialize(item, quantity, price)
+        @item = item
+        @quantity = quantity
+        @price = price    
+    end
+    
+     def select_item
+        case @item
+        when "notebook"
+            Notebook.new(@item,@quantity,@price).compute_total()
+        when "book"
+            Book.new(@item,@quantity,@price).compute_total()
+        when "folder"
+            Folder.new(@item,@quantity,@price).compute_total()
+        else  
+            "We're out of stock"  
+        end
     end
 end
 
-class Invoice < TuitionReport
-    def print
-       puts "Invoice" 
-       puts @name
-       puts @total
+class Notebook < SchoolSupplies
+    def compute_total()
+        @quantity * @price
     end
 end
 
-class Billing < TuitionReport
-    def initialize(name,total,address)
-        @name = name
-        @total = total
-        @address = address
-    end
-
-    def print
-        puts "Billing"
-        puts @name
-        puts @address
-        puts @total
+class Book < SchoolSupplies   
+    def compute_total() 
+        @quantity * @price
     end
 end
-
-invoice = Invoice.new("Ronald", 100)
-invoice.print
-
-billing = Billing.new("Ronald",100,"Bulacan")
-billing.print
+class Folder < SchoolSupplies       
+    def compute_total()
+        @quantity * @price
+    end
+end
+ 
+notebook = SchoolSupplies.new('book',5,20)
+puts notebook.select_item()
